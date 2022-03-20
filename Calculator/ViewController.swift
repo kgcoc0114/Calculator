c8ec457ce68faac3857a88f9f13545c2b3a7eb3e
kgcoc0114 <kgcoc0114@gmail.com>
2022-03-20 23:17:59 +0800

    [Add] Calculator view with auto layout

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var numberButtons: [UIButton]!
+    @IBOutlet var operatorButtons: [UIButton]!
+    @IBOutlet weak var resultLabel: UILabel!
+    var mathExprssion = ""
+    var operatorList = [String]()
+    var clearResult = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        print("123")
         // Do any additional setup after loading the view.
+        for i in 0...9{
+            setCircleButton(button:numberButtons[i])
+        }
+        for i in 0...8{
+            setCircleButton(button:operatorButtons[i])
+        }
+                
     }
+    
+    func setCircleButton(button:UIButton){
+        button.clipsToBounds = true
+        button.layer.cornerRadius = button.bounds.size.height*0.5
+    }
+    
+    @IBAction func numberAction(_ sender: UIButton) {
+        let number = sender.tag
+        if(resultLabel.text == "0"){
+            resultLabel.text = String(number)
+        }else if(clearResult){
+            resultLabel.text = String(number)
+            clearResult=false
+        }
+        else{
+            resultLabel.text = resultLabel.text! + String(number)
+        }
+    }
+    
+    @IBAction func operatorAction(_ sender:UIButton){
+        resultLabel.text="\(resultLabel.text!)+"
+    }
+    
 
-
+    
 }
 
